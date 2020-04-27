850df61ad0b4560183fa680ea5ba04a595b5a147
asoran <altan.soran@gmail.com>
2020-04-28 00:06:38 +0200

    reports lab5

--- lab5/__module-info.java
+++
@@ -1,7 +0,0 @@
-module fr.umlv.structconc {
-
-  requires jdk.incubator.vector;
-
-  requires jmh.core;   // annotations
-  requires static jmh.generator.annprocess;  // only needed at compile time !
-}
